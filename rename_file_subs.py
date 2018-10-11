e693a50453f44134c83bbdcd31eaa94a29c3bb9a
Waldez <waldezjr14@gmail.com>
2018-10-11 11:21:18 +0200

    script to rename file substrings

---
+++ rename_file_subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+if len(sys.argv) != 3:
+	print("usage: rename_file_subs.py OLD_SUBSTRING NEW_SUBSTRING")
+else:
+	print("This script will rename the following files:")
+	file2rename = list()
+	for i, f in enumerate(sorted(os.listdir(os.getcwd()))):
+		if f.find(sys.argv[1]) != -1:
+			file2rename.append(f)
+			print file2rename[i]
+
+	print("***")
+	print("Into:")
+	print("***")
+
+	for f in file2rename:
+		print f.replace(sys.argv[1],sys.argv[2])
+
+	confirm = raw_input("Are you sure? (y/n) ")
+
+	if confirm == "y":
+		for f in file2rename:
+			os.rename(f, f.replace(sys.argv[1],sys.argv[2]))
